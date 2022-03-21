fcd015e61552351efef2aa9522ad822993483f0e
Jason Antman <antmanjt@gmail.com>
2022-03-21 10:59:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const textToChange = document.getElementById('text');
+document.addEventListener('DOMContentLoaded', function () {
+    textToChange.textContent = 'This is really cool!';
+})
+  
