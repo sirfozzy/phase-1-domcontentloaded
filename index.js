a8893728709b63c8be143abc9a2ade4ad918bb95
Jason Antman <antmanjt@gmail.com>
2022-04-11 18:15:42 -0400

    commit tree 7c17dcc5c330fed5018ca41d6c859b80c0267567

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // Your code goes here
-const textToChange = document.getElementById('text');
-document.addEventListener('DOMContentLoaded', function () {
-    textToChange.textContent = 'This is really cool!';
-})
-  
+//const textToChange = document.getElementById('text');
+// document.addEventListener('DOMContentLoaded', function () {
+    // textToChange.textContent = 'This is really cool!';
+// })
+//First, set up a DOMContentLoaded event listener to detect when the HTML page has loaded and the document is ready to be manipulated. 
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('text')
+    //console.log('DOM fully loaded and parsed');
+
+});
+//Use the event's callback function to target the paragraph element with id="text" 
+document.getElementById('text')
+//and replace the text with "This is really cool!"
+spanBye.remove('text')
